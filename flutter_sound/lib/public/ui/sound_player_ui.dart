7037f376f06dab61a5eb0f815845aa00af223092
XuanHanTan <43411204+XuanHanTan@users.noreply.github.com>
2021-03-23 21:23:43 +0800

    UI fixes

--- flutter_sound/lib/public/ui/sound_player_ui.dart
+++ flutter_sound/lib/public/ui/sound_player_ui.dart
@@ -526,7 +526,7 @@ class SoundPlayerUIState extends State<SoundPlayerUI> {
         if (seekPos != null) {
           Log.d("FS --> seeking to  $seekPos");
           await _player.seekToPlayer(seekPos!).whenComplete(() {
-            Future.delayed(Duration(milliseconds: 100), () {
+            Future.delayed(Duration(milliseconds: 250), () {
               setState(() {
                 seekPos = null;
               });
