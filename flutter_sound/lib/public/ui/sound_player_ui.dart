382d57675a0451f6c2fbea0f038f4c3f0713692b
XuanHanTan <43411204+XuanHanTan@users.noreply.github.com>
2021-03-29 21:29:14 +0800

    Bug Fixes

--- flutter_sound/lib/public/ui/sound_player_ui.dart
+++ flutter_sound/lib/public/ui/sound_player_ui.dart
@@ -379,7 +379,7 @@ class SoundPlayerUIState extends State<SoundPlayerUI> {
     if (widget._showTitle && _track != null) rows.add(_buildTitle());
 
     return Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-      SizedBox(width: 60, child: _buildDuration(),),
+      SizedBox(width: 55, child: _buildDuration(),),
       _buildPlayButton(),
       _buildTitle(),
       /*SizedBox(
